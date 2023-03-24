f4e56a617dd62bbccb8008183b7b277e84788c53
metablog <khu10735@naver.com>
2023-03-25 01:09:47 +0900

    css_header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -13,19 +13,20 @@ export default function SearchHeader() {
 
   useEffect(() => setText(keyword || ''), [keyword]);
   return (
-    <header>
-      <Link to="/">
-        <BsYoutube />
-        <h1>YouTube</h1>
+    <header className="w-full flex p-4 border-b border-zinc-600 mb-4">
+      <Link to="/" className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold ml-2 text-3xl">YouTube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="w-full flex justify-center">
         <input
+          className="w-7/12 p-2 outline-none bg-black text-gray-50"
           type="text"
           placeholder="Search..."
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button>
+        <button className="bg-zinc-600 px-4">
           <BsSearch />
         </button>
       </form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#FF0000',
+      },
+    },
   },
   plugins: [],
 };
